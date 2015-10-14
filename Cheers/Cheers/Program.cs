d66b2457b57a8661879a1311d4b42f4d61e4bbf5
Peter Connolly <pmconnolly80@outlook.com>
2015-10-14 11:34:30 -0700

    Commit 2

--- Cheers/Cheers/Program.cs
+++ Cheers/Cheers/Program.cs
@@ -10,6 +10,40 @@ namespace Cheers
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("Hello there! What's your name?");
+            string name = System.Console.ReadLine();
+            System.Console.WriteLine("Hi, " + name);
+            int counter = 0;
+            char[] ans = { 'a', 'e', 'f', 'h', 'i', 'l', 'm', 'n', 'o', 'r', 's', 'x' };
+            foreach (char letter in name)
+            {
+                if (ans.Contains(char.ToLower(letter)))
+                {
+                    System.Console.WriteLine("Give me an " + letter + "\n");
+                }
+                else
+                {
+                    System.Console.WriteLine("Give me a " + letter + "\n");
+                }
+                counter += 1;
+                if (counter == name.Length)
+                {
+                    System.Console.WriteLine(name.ToUpper() + " is GRAND! \n");
+                }
+            }
+
+            System.Console.WriteLine("Press any key to exit");
+            System.Console.ReadKey();
+
+
+            //Get the user's name and print it
+            //Cheer the user on
+            //For example, if name is "Bob", output should be:
+
+            //Give me a..b
+            //Give me an..o
+            //Give me a..b
+            //BOB is.. GRAND!
         }
     }
 }
